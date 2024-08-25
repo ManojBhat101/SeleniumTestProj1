da24de3a225f2b2fc31fb75881239735934efc29
Admin <Admin@DESKTOP-MICJN7G>
2024-08-25 18:06:56 +0530

    Updated mavenDemo.java file and pom.xml file

--- com.learnautomation.selenium/src/test/java/com/learnautomation/selenium/mavenDemo.java
+++ com.learnautomation.selenium/src/test/java/com/learnautomation/selenium/mavenDemo.java
@@ -2,14 +2,27 @@ package com.learnautomation.selenium;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.Assert;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class mavenDemo {
-	
+
+	@Parameters("Browser")
 	@Test
-	public void testHello() {
-		WebDriver driver = new ChromeDriver();
+	public void testHello(String browserName) 
+	{
+		System.out.println("Parameter value is "+browserName);
+		WebDriver driver=null;
+		if(browserName.contains("Chrome"))
+		{
+			driver = new ChromeDriver();
+		}
+		else if(browserName.contains("Edge"))
+		{
+			driver = new EdgeDriver();
+		}
 		driver.manage().window().maximize();
 		driver.get("https://online.actitime.com/sshakti/login.do");
 		Assert.assertTrue(driver.getTitle().contains("actiTIME"),"Title doesnot match");
